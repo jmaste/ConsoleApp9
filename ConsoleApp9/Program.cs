36dcb16b74b6e5188ac74bd191480ac866577baa
jmaste <jmaste00@fesb.hr>
2021-06-17 18:21:29 +0200

    Promjena

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp9
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
+
+
         }
     }
 }
